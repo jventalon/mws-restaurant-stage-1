a57ea59ee9a963acaaedf7cc9bff0eca8a02dfb4
Justine Ventalon <Justine Ventalon>
2018-06-18 21:53:18 +0200

    fix: Change maps to Leaflet instead of Google Maps

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 let restaurants,
     neighborhoods,
     cuisines;
-var map;
+var newMap;
 var markers = [];
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+    initMap();
     fetchNeighborhoods();
     fetchCuisines();
 });
@@ -68,19 +69,24 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 }
 
 /**
- * Initialize Google map, called from HTML.
+ * Initialize Leaflet map, called from HTML.
  */
-window.initMap = () => {
-    let loc = {
-        lat: 40.722216,
-        lng: -73.987501
-    };
-    self.map = new google.maps.Map(document.getElementById('map'), {
+initMap = () => {
+  self.newMap = L.map('map', {
+        center: [40.722216, -73.987501],
         zoom: 12,
-        center: loc,
-        scrollwheel: false
-    });
-    updateRestaurants();
+        scrollWheelZoom: false
+      });
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+    mapboxToken: 'pk.eyJ1IjoianZlbnRhbG9uIiwiYSI6ImNqaWttMDMxOTJmaHgza3BlajdxZ3dlOTMifQ.DqcTWxGIVgOzYMU9pxMiyA',
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(newMap);
+
+  updateRestaurants();
 }
 
 /**
